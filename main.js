72d35970c3b2ae17f73279001ca319f3846dbdea
angelina <balassoneangelina@gmail.com>
2022-04-29 15:07:24 -0300

    mi primer archivo del repositorio

---
+++ main.js
@@ -0,0 +1,161 @@
+
+/* Ejercicio 1 
+    let nombre = "Homero";
+    let apellido = "Simpsons";
+    let edad = "53";
+    console.log(nombre);
+    console.log(apellido);
+    console.log(edad); */
+
+    // Ejercicio 2//
+    const ciudad1 = "Roma";
+    const ciudad2 = "MardelPlata";
+    const ciudad3 = "Paris";
+    const ciudad4 = "Junin"
+    const ciudad5 = "RiodeJaneiro"; 
+    console.log(ciudad1);
+    console.log(ciudad2);
+    console.log(ciudad3);
+    console.log(ciudad4);
+    console.log(ciudad5);
+
+    // Ejercicio 3// 
+    let nombre1 = "Bart";
+    let apellido1 = "Simpsons";
+    let direccion = " 742 evergreen terr";
+    let carnet = nombre1+" "+apellido1+" " + direccion;
+    console.log(carnet);
+
+    /* Ejercicio 4
+    let familiar1 = prompt ("Ingresar nombre de familiar 1");
+    let familiar2 = prompt ("Ingresar nombre de familiar 2");
+    let familiar3 = prompt ("Ingresar nombre de familiar 3");
+    let familiar4 = prompt ("Ingresar nombre de familiar 4");
+    let familia5 = prompt ("Ingresar nombre de familiar 5");
+    let familiares = familiar1 + " " + familiar2 + " " + familiar3 + " " + familiar4 + " " + familia5;
+    alert('familiares'); */
+
+
+    /* Ejercicio 5 
+    let precio = prompt ("Ingresar un precio");
+    let precio1 = prompt ("Ingresar otro precio");
+    let descuento = 0.70; 
+    let precioFinal = precio * descuento;
+    let precioFinal1 = precio1 * descuento;
+    console.log();('precioFinal1'); */
+
+    /* Ejercicio 1 Segunda clase 
+    let nombre = "Juan";
+    let ingresar = prompt ("Ingresar un nombre");
+    if (ingresar == nombre){
+        alert('Fui yo');
+    }else{
+        alert('Yo no fui');
+    } */
+
+    /* Ejercicio 2 Segunda clase 
+    let ingresar = prompt ("Ingresar una letra");
+    if ((ingresar == "y") ||  (ingresar == "Y")) {
+        alert ("Correcto");
+    }
+        else {
+            alert ("Error");
+        } 
+    */
+   /* Ejercicio 3 
+   let numero = prompt ("Ingresar un numero");
+   if (numero == 1) {
+       alert ("elegiste a Homero");}
+    else if (numero ==2) {
+        alert ("elegiste a Marge");
+    }
+    else if (numero ==3) {
+        alert ("elegiste a Bart");}
+    else if (numero==4) {
+        alert ("elegiste a Lisa");
+   } 
+   else { alert ("error") };
+   */
+  /*Ejercicio 4
+let numero =  parseFloat (prompt ("Ingresar un numero"));
+if ((numero >= 0) && (numero <= 1000)) {
+alert ("Presupuesto bajo");
+}
+else if ((numero >= 1001) && (numero <=3000)) {
+alert ("Presupuesto medio");}
+else if (numero>3000) {
+    alert ("Prespuesto alto");
+}
+else { 
+    alert ("error");
+}
+*/
+/*Ejercicio 5
+let producto1 = prompt ("Ingresar 1 producto");
+let producto2 = prompt ("Ingresar 2 producto de almacen");
+let producto3 = prompt ("Ingresar 3 producto de almacen");
+let producto4 = prompt ("Ingresar 4 producto de almacen");
+if ((producto1 != " ") && (producto2 != " ") && (producto3 != " ") && (producto4 != " ")); {
+    alert (producto1+producto2+producto3+producto4);
+}
+else { 
+    alert ("error");
+}
+*/
+/* PARTE 3 Ejercicio 1  
+let cantidad = prompt('INGRESAR NUMERO DE REPETICIONES');
+let texto    = prompt('INGRESAR TEXTO A REPETIR');
+for (let index = 0; index < cantidad; index++) {
+    alert(texto);
+}
+*/
+
+/* PARTE 3 Ejercicio 2
+let numero = prompt("Ingresar un numero");
+for (let i= numero; i<= 4; i++) {
+    alert ("lado")
+    break;}
+    */
+
+    // PARTE 3 Ejercicio 3 //
+    let productos = '';
+    for (let index = 0; index < 5; index++) {
+        productos += prompt("INGRESAR PRODUCTOS")+"\n";
+    }
+    alert(productos);
+    
+
+    /*let entrada    = prompt("INGRESAR NOMBRE");
+    let ingresados = '';
+    while (entrada != 'Voldemort') {
+        ingresados += entrada +"\n";
+        entrada     = prompt("INGRESAR NOMBRE");
+    }
+    alert(ingresados); 
+    */
+ 
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+   
+    
+
+
+
+
