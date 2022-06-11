d7a0063a68052f49bed78b1b090d8789b5d62a17
angebalassone <balassoneangelina@gmail.com>
2022-06-11 13:45:57 -0300

    Septima entrega

--- js/main.js
+++ js/main.js
@@ -21,55 +21,9 @@ if (usuario == null) {
         }
         //return nombre;
       };
-    
-    
-      
-   /*class Productos {
-      constructor (nombre, precio, articulo) {
-       this.nombre = nombre;
-       this.precio = precio;
-       this.articulo = articulo;
-       };
-      };
-      let productoArticulo;
-      let carrito = [];
-    
-    const almohadon1 = new Productos ("Beautiful", 700, 1);
-    const almohadon2 = new Productos ("Enjoy Life", 700, 2);
-    const almohadon3 = new Productos ("Piel Berrex", 800, 3);
-    const productos = [almohadon1,almohadon2,almohadon3];
-    
-    const mostrarProductos = () => {
-    let texto = "";
-    productos.forEach((element) => {
-    texto += `${element.articulo}) ${element.nombre}\n`;
-    });
-    let seleccion = parseInt(prompt(`Que almohadon llevara? :\n${texto}`));
-    while (seleccion < 1 || seleccion > 3) {
-     seleccion = parseInt(
-    prompt(`Incorrecto !\nQue almohadon llevara? :\n${texto}`));
-    }
-    return seleccion;
-    };
-    
-    const productoSeleccionado = (articulo) => {
-      console.log(articulo);
-      let productosFind = productos.find((element) => element.articulo === articulo);
-      carrito.push(productosFind);
-      let seguir = confirm("Desea llevar otro almohadon ?");
-    
-      if (seguir === true) {
-      init();
-      }
-    };
-    
-    const init = () => {
-    productoArticulo = mostrarProductos ();
-    productoSeleccionado (productoArticulo);
-    };*/
-    
+   
     saludar (); 
-    //init ();//
+    
    
     // Operadon o //
     let entrada2 = prompt("INGRESAR SI TENES UN CODIGO DE DESCUENTO");
@@ -78,5 +32,46 @@ if (usuario == null) {
     } else {
     console.log("ERROR");
     }
-        
 
+    //Promesa de clase//
+    const BD = [
+      {id: 1, nombre: 'Almohadon Enjoy Life', precio: 700},
+      {id: 2, nombre: 'Almohadon Dreams White', precio: 800},
+      {id: 3, nombre: 'Almohadon Love Celeste ', precio: 900},
+      
+     ]
+     const pedirProductos = () => {
+      return new Promise( (resolve, reject) => {
+      setTimeout(() => {
+      resolve(BD)
+      }, 3000)
+      })
+    }
+
+    
+let productos = []
+const renderProductos = (arr) => {
+ 
+ for (const item of arr) {
+   console.log(item);
+ }
+}
+
+pedirProductos()
+ .then((res) => {
+ productos = res
+ renderProductos(productos)
+ })
+ 
+    
+// Ajax//
+$.ajax({
+  method: "GET",
+  url: "http://file:///C:/Users/angelina.balassone/Desktop/backup%20Angi/ANGE%20NUEVA%20COMPU/ange/angelina/curso%20de%20desarrolador%20web/Javascript/TRABAJO/nuevo_repo/index.html/"
+}).done(function(data) {
+  alert(data); // imprimimos la respuesta
+}).fail(function() {
+  alert("Algo salió mal");
+}).always(function() {
+  alert("Siempre se ejecuta")
+});
